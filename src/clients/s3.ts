33b898a9ef682ecd79534774fefb5a89a58f839d
Michał Mrozek <michalmrozek@wp.pl>
2021-10-21 16:22:52 +0200

    fix: missing aws region

--- src/clients/s3.ts
+++ src/clients/s3.ts
@@ -1,4 +1,3 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import { region } from '../utils/aws';
 
-export const s3Client = new S3Client({ region });
+export const s3Client = new S3Client({});
