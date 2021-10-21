7cd21855eb120e2011c317345a769e68b5a354b5
Michał Mrozek <michalmrozek@wp.pl>
2021-10-21 16:06:42 +0200

    fix: use promisify, instead of fs/promises

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,14 +1,15 @@
-import { readdir, stat } from 'fs/promises';
+import { readdir, stat } from 'fs';
 import { join } from 'path';
+import { promisify } from 'util';
 
 export const readRecursively = async (dir: string): Promise<string[]> => {
-  const dirContent = await readdir(dir);
+  const dirContent = await promisify(readdir)(dir);
 
   return dirContent.reduce(async (foundFiles, fileOrDir) => {
     const path = join(dir, fileOrDir);
     const files = await foundFiles;
 
-    if ((await stat(path)).isDirectory()) {
+    if ((await promisify(stat)(path)).isDirectory()) {
       return files.concat(await readRecursively(path));
     }
 
